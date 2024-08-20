bcd369575db12819b1526c754c59d5b4114a35f2
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-08-20 18:40:12 +0300

    remap scroll_docs

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -45,8 +45,8 @@
           "<C-n>" = "cmp.mapping.select_next_item()";
           "<C-p>" = "cmp.mapping.select_prev_item()";
           "<C-e>" = "cmp.mapping.abort()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-u>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<S-CR>" =
