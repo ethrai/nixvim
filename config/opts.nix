3fee9141a6c4b42d6b1142d6a6c6abaee0fe1b87
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-08-22 04:03:22 +0300

    return this crap back

--- config/opts.nix
+++ config/opts.nix
@@ -20,6 +20,12 @@
           }
       })
     '';
+    globals = {
+      # Disable useless providers
+      loaded_ruby_provider = 0; # Ruby
+      loaded_perl_provider = 0; # Perl
+      loaded_python_provider = 0; # Python 2
+    };
 
     clipboard = {
       # Use system clipboard
