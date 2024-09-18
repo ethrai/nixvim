412f01f3817b899e1371ba72f2927cc9d3cc188f
Sergei Ryzhko <ryzhkosergej@gmail.com>
2024-09-18 16:31:02 +0300

    try to fix jdtls

--- config/plugins/java.nix
+++ config/plugins/java.nix
@@ -1,8 +1,12 @@
-{
-  plugins.lsp.servers.jdt-language-server.enable = true;
+{ config, pkgs, inputs, ... }: {
+  plugins.lsp.servers.jdt-language-server = {
+    enable = true;
+    package = pkgs.jdt-language-server;
+    cmd = [ "${pkgs.jdt-language-server}/bin/jdtls" ];
+  };
   plugins.nvim-jdtls = {
     enable = true;
-    data = "/home/sergio/.cache/jdtls/workspace";
-    configuration = "/home/sergio/.cache/jdtls/config";
+    data = "${config.home.homeDirectory}/.cache/jdtls/workspace";
+    configuration = "${config.home.homeDirectory}/.cache/jdtls/config";
   };
 }
